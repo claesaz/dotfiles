cef796a2878215581eb47c632a31925264e939d8
Claes Persson <c@cp.se>
2024-10-12 13:23:55 +0200

    neovim: updates

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -92,20 +92,24 @@ require("lazy").setup({
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			-- Document existing key chainsrequire
+			require("which-key").add({
+				{ "", group = "[S]earch" },
+				{ "", desc = "<leader>r_", hidden = true },
+				{ "", desc = "<leader>s_", hidden = true },
+				{ "", desc = "<leader>t_", hidden = true },
+				{ "", group = "[T]oggle" },
+				{ "", group = "[W]orkspace" },
+				{ "", group = "[R]ename" },
+				{ "", group = "Git [H]unk" },
+				{ "", group = "[C]ode" },
+				{ "", desc = "<leader>h_", hidden = true },
+				{ "", desc = "<leader>c_", hidden = true },
+				{ "", desc = "<leader>d_", hidden = true },
+				{ "", group = "[D]ocument" },
+				{ "", desc = "<leader>w_", hidden = true },
+				{ "", desc = "<leader>h", mode = "v" },
 			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 	{
