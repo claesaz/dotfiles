fc08a3f3d59b3dc473f5b441963c6d03fb4139e6
Claes Persson <c@cp.se>
2024-12-30 00:33:51 +0100

    removing copilot

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -60,16 +60,16 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"github/copilot.vim",
-		config = function()
-			vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
-				expr = true,
-				replace_keycodes = false,
-			})
-			vim.g.copilot_no_tab_map = true
-		end,
-	},
+	--{
+	--	"github/copilot.vim",
+	--	config = function()
+	--		vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+	--			expr = true,
+	--			replace_keycodes = false,
+	--		})
+	--		vim.g.copilot_no_tab_map = true
+	--	end,
+	--},
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	{
 		"numToStr/Comment.nvim", -- Comments
